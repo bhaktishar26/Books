bfb1b131a8169d39c831788923f3b36c6490a9ef
bhaktishar26 <bhakti.sharma_cs18@gla.ac.in>
2019-09-20 23:55:19 +0530

    added getter and setter methods

--- src/bookStore/BookStore.java
+++ src/bookStore/BookStore.java
@@ -4,4 +4,30 @@ public class BookStore {
     private String bookName;
     private String authorName;
     private String isbn;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName() {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName() {
+        this.authorName = authorName;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn() {
+        this.isbn = isbn;
+    }
+
+
 }
